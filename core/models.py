de6d664e2add259ab598cc8b62e97a41f556cdbd
Rhuan Nascimento <Rhuan.g.nascimento@gmail.com>
2023-12-20 17:56:32 -0300

    Organizando

--- core/models.py
+++ core/models.py
@@ -6,9 +6,12 @@ class Cliente(models.Model):
     telefone = models.CharField('Telefone', max_length=15)
     dataEntrada = models.DateField('Data de Entrada')
     dataUltimAtualiza = models.DateField('Ultima atualização')
-    contatoInicial = models.BooleanField('Contato inicial')
-    situacao = models.BooleanField('Situação')
+    contatoInicial = models.BooleanField('Contato bem sucedido')
+    situacao = models.BooleanField('Conversando')
     andamento = models.CharField('Andamento', max_length=200)
+
+    def __str__(self):
+        return f'{self.telefone} {self.nome}'
 
 class Imovel (models.Model):
     residencial = models.CharField('Residencial', max_length=50)
@@ -16,3 +19,6 @@ class Imovel (models.Model):
     proprietario = models.CharField('Proprietário', max_length=100)
     telefone = models.CharField('Telefone', max_length=15)
     midia = models.ImageField('Fotos')    
+
+    def __str__(self):
+        return f'{self.residencial} {self.proprietario}'
